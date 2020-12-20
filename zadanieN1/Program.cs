f2874040160a9ebe7c3dd1d15aa34fcade1e1a44
sarvilinv <76404262+sarvilinv@users.noreply.github.com>
2020-12-20 23:42:06 +0500

    q

---
+++ zadanieN1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+
+namespace zadanieN1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+
+            Console.Write("Введите длину массива: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] array = new int[n];
+
+            for (int i = 0; i != n; i++)
+            {
+                array[i] = rnd.Next(-100,100);
+            }
+            string path = @"D:\test.txt";
+
+            using (StreamWriter sw = new StreamWriter(path, false, System.Text.Encoding.Default))
+            {
+                for (int i = 0; i!= n; i++)
+                {
+                    sw.WriteLine(array[i]);
+                }
+            }
+        }
+    }
+}
